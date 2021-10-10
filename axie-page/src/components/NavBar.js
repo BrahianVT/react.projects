f258a1bedc993d5034aea9c59ff5cb52e2969de3
BrahianVT <pumasemj@hotmail.com>
2021-10-09 22:15:05 -0600

    created navbar

---
+++ axie-page/src/components/NavBar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import {AppBar, Toolbar, IconButton, Typography} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import logo from "../assets/img/branding.jpg";
+const useStyles= makeStyles(()=>({
+    root:{
+        flexGrow: 1
+    },
+    menuButton:{
+        marginRight: '16px'
+    },
+    title:{
+        flexGrow: 1
+    },
+    imagen:{
+        borderRadius: '50%'
+    }
+    }));
+
+function NavBar() {
+    const classes= useStyles();
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                       BorjaScript
+                    </Typography>
+
+                    <IconButton color="inherit">
+                            <img src={logo} width="40px" height="40px" alt="" className={classes.imagen}/>
+                            </IconButton>
+                </Toolbar>
+
+            </AppBar>
+            
+        </div>
+    );
+}
+
+export default NavBar;
--- axie-page/src/pages/DashBoard.js
+++ axie-page/src/pages/DashBoard.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import NavBar from "../components/NavBar";
+
+const useStyles = makeStyles( () =>({
+    root:{
+        flexGrow:1
+    }
+
+}));
 
 function DashBoard(props) {
+    const classes = useStyles();
     return (
-      <div>
-        DashBoard Component
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <NavBar/>
+            </Grid>
+        </Grid>
+
       </div>
     );
   }
