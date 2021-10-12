ab54f908895ffa58b29bba947e9cc7e9d1a54e18
BrahianVT <pumasemj@hotmail.com>
2021-10-12 12:15:00 -0600

    added first commit

---
+++ axiez/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {BrowserRouter, Switch, Route } from "react-router-dom";
+import Main from "./pages/Main";
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/"  component = {Main}/>
+        </Switch>
+      </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ axiez/src/pages/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Main(){
+    return (
+        <div> DashBoard here</div>
+    );
+}
+
+export default Main;
