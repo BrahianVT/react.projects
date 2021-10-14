c74c10e3bf2c6d5bf1541dd201e26e66bd199c33
BrahianVT <pumasemj@hotmail.com>
2021-10-13 23:22:27 -0600

    added all elements

--- axiez/src/components/HBar.js
+++ axiez/src/components/HBar.js
@@ -1,12 +1,54 @@
-import React from "react";
+import * as React from "react";
 import { List, ListItem, ListItemText, Checkbox } from '@mui/material';
-import { green, red, orange, pink, purple, grey, black ,blue } from '@mui/material/colors';
+import { green, red, orange, pink, purple, grey ,blue } from '@mui/material/colors';
+import { Grid  ,Slider} from '@mui/material';
+import MuiInput from '@mui/material/Input';
 import { useHistory, useParams } from 'react-router-dom'
-                
+import { styled } from '@mui/material/styles';
+
+const Input = styled(MuiInput)`
+  width: 40px;
+`;
 function Hbar(){
-    const [classes, setClasses] = React.useState(new Set());
-    const [queryClasses, setQueryClasses] = React.useState("");
-    const history = useHistory()         
+    const [classes, setClasses] = React.useState(new Set())
+    const [queryClasses, setQueryClasses] = React.useState("")        
+    const [valueHp, setValueHp] = React.useState(27)
+    const [valueSpeed, setValueSpeed] = React.useState(27)
+    const [valueSkill, setValueSkill] = React.useState(27)
+    const [valueMoral, setValueMoral] = React.useState(27)
+    const [valueBreed, setValueBreed] = React.useState(7)
+    const [valueMystic, setValueMystic] = React.useState(0)
+    const [valuePure, setValuePure] = React.useState(1)                
+    const history = useHistory()
+    const handleSliderChange = (event, newValue) => {
+          if(event.target.name === '1')setValueHp(newValue);
+          if(event.target.name === '2')setValueSpeed(newValue);
+          if(event.target.name === '3')setValueSkill(newValue);
+          if(event.target.name === '4')setValueMoral(newValue);
+          if(event.target.name === '5')setValueBreed(newValue);
+          if(event.target.name === '6')setValueMystic(newValue);
+          if(event.target.name === '7')setValuePure(newValue);
+    };
+
+    const handleInputChange = (event) => {
+          if(event.target.name === '1')setValueHp(event.target.value === '' ? '' : event.target.valueAsNumber);
+          if(event.target.name === '2')setValueSpeed(event.target.value === '' ? '' : event.target.valueAsNumber > 61?61:event.target.valueAsNumber);
+          if(event.target.name === '3')setValueSkill(event.target.value === '' ? '' : event.target.valueAsNumber > 61?61:event.target.valueAsNumber);
+          if(event.target.name === '4')setValueMoral(event.target.value === '' ? '' : event.target.valueAsNumber > 61?61:event.target.valueAsNumber);
+          if(event.target.name === '5')setValueBreed(event.target.value === '' ? '' : event.target.valueAsNumber > 7?7:event.target.valueAsNumber);
+          if(event.target.name === '6')setValueMystic(event.target.value === '' ? '' : event.target.valueAsNumber > 6?6:event.target.valueAsNumber);
+          if(event.target.name === '7')setValuePure(event.target.value === '' ? '' : event.target.valueAsNumber > 6?6:event.target.valueAsNumber);
+    };
+   
+    const handleBlur = (event) => {
+          setValueHp(valueHp < 21 ?21: valueHp > 61?61:valueHp); 
+          setValueSpeed(valueSpeed < 21 ?21: valueSpeed > 61?61:valueSpeed);
+          setValueSkill(valueSkill < 21 ?21: valueSkill > 61?61:valueSkill);
+          setValueMoral(valueMoral < 21 ?21: valueMoral > 61?61:valueMoral);
+          setValueBreed(valueBreed < 0 ?0: valueBreed > 7?7:valueBreed);
+          setValueMystic(valueBreed < 0 ?0: valueBreed > 7?7:valueBreed);
+          setValueBreed(valueBreed < 0 ?0: valueBreed > 7?7:valueBreed);
+     };
     const handleChange = (event) => { 
         switch(event.target.id){
            case "plant":
@@ -66,15 +108,53 @@ function Hbar(){
         } else {
            params.delete("classes");                    
         }
+
+        if(valueHp > 27){
+           params.append("hp", valueHp);                    
+        } else {
+          params.delete("hp");                   
+        }
+        if(valueSpeed > 27){
+           params.append("speed", valueSpeed);                    
+        } else {
+          params.delete("speed");                   
+        }
+       if(valueSkill > 27){
+           params.append("skill", valueSkill);                    
+        } else {
+          params.delete("skill");                   
+        } 
+        if(valueMoral > 27){
+           params.append("moral", valueMoral);                    
+        } else {
+          params.delete("moral");                   
+        }
+        if(valueBreed < 7){
+           params.append("breeds", valueBreed);                    
+        } else {
+          params.delete("breeds");                   
+        }
+        if(valueMystic > 0){
+           params.append("mystics", valueMystic);                    
+        } else {
+          params.delete("mystics");                   
+        }
+        if(valuePure > 1){
+           params.append("pureness", valuePure);                    
+        } else {
+          params.delete("pureness");                   
+        }
+
         
         history.push({ search: params.toString()});
-     }, [classes, history, queryClasses])
+     }, [classes, history, queryClasses, valueHp, valueSpeed, valueSkill, valueMoral, valueMystic, valueBreed, valuePure])
 
      const { params2 } = useParams()
         console.log("Parametros " + params2)
         
     return (
-       <List sx={{  maxWidth: 300, bgcolor: 'background.paper' }}>
+       <List sx={{  maxWidth: 320, bgcolor: 'background.paper' }}>
+          <ListItemText primary={"Class"} />                 
          <ListItem key='tanks' disablePadding={true} >   
                 <Checkbox  sx={{ color: green[600], '&.Mui-checked': { color: green[600] },paddingRight: 0}} 
                   id='plant' checked={classes.has("plant")} onChange={handleChange}
@@ -86,7 +166,6 @@ function Hbar(){
                   inputProps={{ 'aria-labelledby': "controlled" }}
                 />
               Beast
-
               <Checkbox  sx={{ color: blue[600], '&.Mui-checked': { color: blue[600] },paddingRight: 0}} 
                   id='aqua' checked={classes.has("aqua")} onChange={handleChange}
                   inputProps={{ 'aria-labelledby': "controlled" }}
@@ -128,7 +207,82 @@ function Hbar(){
                   inputProps={{ 'aria-labelledby': "controlled" }}
                 />
               Bug
-          </ListItem>                                 
+          </ListItem>
+          <ListItemText primary={"Stats"} />
+         <ListItem sx={{ padding:.4}}>
+          <Grid item xs sx={{ paddingRight:3}}>
+             <Slider  name="1" sx={{ color: green[600]}} min = {27} max = {61} value={typeof valueHp === 'number' ? valueHp : 0}
+            onChange={handleSliderChange} aria-labelledby="input-slider" />
+        </Grid>
+           <Grid item>
+             <Input value={valueHp} size="small" onChange={handleInputChange} onBlur={handleBlur} name="1"
+              inputProps={{ step: 1, min: 27, max: 61, type: 'number', 'aria-labelledby': 'input-slider',}} />
+           </Grid>
+           </ListItem>
+           <ListItem sx={{ padding:.4}}>      
+           <Grid item xs sx={{ paddingRight:3}}>
+             <Slider name="2" sx={{ color: orange[600]}} min = {27} max = {61} value={typeof valueSpeed === 'number' ? valueSpeed : 0}
+            onChange={handleSliderChange} aria-labelledby="input-slider" />
+        </Grid>
+           <Grid item>
+             <Input value={valueSpeed} size="small" onChange={handleInputChange} onBlur={handleBlur} name="2"
+              inputProps={{ step: 1, min: 27, max: 61, type: 'number', 'aria-labelledby': 'input-slider',}} />
+           </Grid>
+           </ListItem>
+           <ListItem sx={{ padding:.4}}>      
+           <Grid item xs sx={{ paddingRight:3}}>
+             <Slider name="3" sx={{ color: purple[600]}} min = {27} max = {61} value={typeof valueSkill === 'number' ? valueSkill : 0}
+            onChange={handleSliderChange} aria-labelledby="input-slider" />
+        </Grid>
+           <Grid item>
+             <Input value={valueSkill} size="small" onChange={handleInputChange} onBlur={handleBlur} name='3'
+              inputProps={{ step: 1, min: 27, max: 61, type: 'number', 'aria-labelledby': 'input-slider',}} />
+           </Grid>
+           </ListItem>   
+           <ListItem sx={{ padding:.4}}>   
+           <Grid item xs sx={{ paddingRight:3}}>
+             <Slider name="4" sx={{ color: red[800]}} min = {27} max = {61} value={typeof valueMoral === 'number' ? valueMoral : 0}
+            onChange={handleSliderChange} aria-labelledby="input-slider" />
+        </Grid>
+           <Grid item>
+             <Input value={valueMoral} size="small" onChange={handleInputChange} onBlur={handleBlur} name="4"
+              inputProps={{ step: 1, min: 27, max: 61, type: 'number', 'aria-labelledby': 'input-slider',}} />
+           </Grid>
+          </ListItem>
+
+          <ListItemText primary={"Breeds"} />
+         <ListItem sx={{ padding:.4}}>
+          <Grid item xs sx={{ paddingRight:3}}>
+             <Slider name="5" min = {0} max = {7} value={typeof valueBreed === 'number' ? valueBreed : 0}
+            onChange={handleSliderChange} aria-labelledby="input-slider" />
+        </Grid>
+           <Grid item>
+             <Input value={valueBreed} size="small" onChange={handleInputChange} onBlur={handleBlur} name="5"
+              inputProps={{ step: 1, min: 0, max: 7, type: 'number', 'aria-labelledby': 'input-slider',}} />
+           </Grid>
+           </ListItem>
+         <ListItemText primary={"Mystic"} />
+         <ListItem sx={{ padding:.4}}>
+          <Grid item xs sx={{ paddingRight:3}}>
+             <Slider name="6" min = {0} max = {6} value={typeof valueMystic === 'number' ? valueMystic : 0}
+            onChange={handleSliderChange} aria-labelledby="input-slider" />
+        </Grid>
+           <Grid item>
+             <Input value={valueMystic} size="small" onChange={handleInputChange} name="6"
+              inputProps={{ step: 1, min: 0, max: 6, type: 'number', 'aria-labelledby': 'input-slider',}} />
+           </Grid>
+           </ListItem>
+           <ListItemText primary={"Pureness"} />
+         <ListItem sx={{ padding:.4}}>
+          <Grid item xs sx={{ paddingRight:3}}>
+             <Slider name="7" min = {1} max = {6} value={typeof valuePure === 'number' ? valuePure : 0}
+            onChange={handleSliderChange} aria-labelledby="input-slider" />
+        </Grid>
+           <Grid item>
+             <Input value={valuePure} size="small" onChange={handleInputChange} name="7" 
+              inputProps={{ step: 1, min: 1, max: 6, type: 'number', 'aria-labelledby': 'input-slider',}} />
+           </Grid>
+           </ListItem>                                                 
        </List>                    
     );
 }
